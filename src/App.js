c5ba0073ab227ae3c7816caf2f3a61fd198df847
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-07 22:05:30 -0700

    updated App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Navbar from './components/Navbar';
 export default function App() {
   return (
     <Router>
-      <div className="font-sans text-gray-800">
+      <div className="app-container">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
