396fa8f8381851255e3afb31e41c1c80b64c97e7
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-07 22:06:11 -0700

    updated navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 export default function Navbar() {
   return (
-    <nav className="bg-white sticky top-0 z-50 shadow-md px-6 py-4 flex justify-between items-center">
-      <Link to="/" className="text-2xl font-extrabold text-blue-600 tracking-tight">ReactBlog</Link>
-      <div className="space-x-4">
-        <Link to="/" className="text-gray-600 hover:text-blue-500 transition">Home</Link>
-        <Link to="/create" className="text-gray-600 hover:text-blue-500 transition">Create Post</Link>
+    <nav className="navbar">
+      <Link to="/" className="logo">ReactBlog</Link>
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/create">Create Post</Link>
       </div>
     </nav>
   );
