83269736f2c51efa4c7d1a3b56116d817e0ac5f4
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-07 21:31:54 -0700

    added data/post.js

---
+++ src/data/post.js
@@ -0,0 +1,21 @@
+const posts = [
+  {
+    id: 1,
+    title: 'First Blog Post',
+    excerpt: 'This is the excerpt of the first post...',
+    content: 'This is the full content of the first post. Welcome to our React blog!'
+  },
+  {
+    id: 2,
+    title: 'Learning React Router',
+    excerpt: 'How to use React Router in simple steps...',
+    content: 'React Router helps us create SPA navigation easily...'
+  },
+  {
+    id: 3,
+    title: 'Why TailwindCSS Rocks',
+    excerpt: 'TailwindCSS is a utility-first CSS framework...',
+    content: 'With TailwindCSS you write less CSS and build modern responsive designs faster...'
+  }
+];
+export default posts;
